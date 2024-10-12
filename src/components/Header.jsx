d49d52f8ae4d55f59203a3c9933bea7111dedfa0
Jonash189 <jonas.hellstrom89@gmail.com>
2024-10-12 14:20:29 +0200

    Adding mediaQ so its one albumcover in mobile and two in ipads.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
     return (
         <div>
-            <h1>Spotify lista</h1>
+            <h1>Music releases</h1>
         </div>
     );
 
