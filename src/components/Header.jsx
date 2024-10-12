779ece8405d591401e05e3a69c9bf0d8ee81c032
Jonash189 <jonas.hellstrom89@gmail.com>
2024-10-12 14:45:15 +0200

    Styling header and h1.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './Header.css';
 
 export const Header = () => {
     return (
-        <div>
+        <div className="header">
             <h1>Music releases</h1>
         </div>
     );
